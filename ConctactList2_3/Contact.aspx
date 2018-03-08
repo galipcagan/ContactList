cb3bfd5929e11d6b9ca425301b5e70a34d300e50
larug42 <rcagan42@hotmail.com>
2018-03-08 00:29:03 -0500

    First Commit

---
+++ ConctactList2_3/Contact.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Contact.aspx.cs" Inherits="Contact" %>
+
+<!DOCTYPE html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<script>
+    function isNumberKey(evnt) {
+        var CharCode = (evnt.which) ? evnt.which : evnt.keyCode;
+        if (CharCode > 31 && (CharCode < 48) || CharCode > 57)){
+            return (alert("yes"));
+        }
+
+        return (alert("no"));
+    }
+</script>
+<head runat="server">
+    <title>Contact List</title>
+</head>
+<body>
+ <div class="container-fluid">
+     <div class="row">
+         <div class="col-xs-2"> </div>
+         <div class="col-xs-10"> 
+    <form id="form1" runat="server">
+        <div>
+            <asp:HiddenField ID="hf" runat="server" />
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="First Name"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtFirst" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text="Last Name"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtLast" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label3" runat="server" Text="Phone Number"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtPhone" runat="server" onkeypress="return isNumberKey(event)"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td colspan="2">
+                        <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                        <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td colspan="2">
+                        <asp:Label ID="lblSuccessMessage" runat="server" Text=""></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td colspan="2">
+                        <asp:Label ID="lblErrorMessage" runat="server" Text=""></asp:Label>
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:GridView ID="gvCotact" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="First" HeaderText="First Name" />
+                    <asp:BoundField DataField="Last" HeaderText="Last Name" />
+                    <asp:BoundField DataField="Phone" HeaderText="Phone Number" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton ID="lnkView" runat="server" CommandArgument='<%# Eval("id") %>' OnClick="lnkOnClick" >View</asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+
+   </div>
+  </div>
+ </div>
+</body>
+</html>
---
+++ ConctactList2_3/Contact.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient; //used to do connections
+using System.Data;
+
+public partial class Contact : System.Web.UI.Page
+{
+    SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\derya.DESKTOP-BHDIT4P\Source\Repos\ConctactList2_3\ConctactList2_3\App_Data\Database.mdf;Integrated Security=True");
+    protected void Page_Load(object sender, EventArgs e) //generic loading page
+    {
+        if (!IsPostBack) //if rendered first time or not 
+        {
+            btnDelete.Enabled = false;
+            FillGridView();
+        }
+    }
+
+    protected void btnClear_Click(object sender, EventArgs e) //button that clears fields
+    {
+        clear(); 
+    }
+    public void clear() { // as it will be called again use it.
+        hf.Value = "";
+        txtFirst.Text = txtLast.Text = txtPhone.Text = "";
+        lblErrorMessage.Text = lblSuccessMessage.Text = "";
+        btnSave.Text = "Save";
+        btnDelete.Enabled = false;
+
+    }
+
+    protected void btnSave_Click(object sender, EventArgs e) //function used to create or update db
+    {
+        if (sqlCon.State == ConnectionState.Closed) {
+            sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand("ContactCreateOrUpdate", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@Id",hf.Value==""?0:Convert.ToInt32(hf.Value));
+            sqlCmd.Parameters.AddWithValue("@First",txtFirst.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@Last", txtLast.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@Phone", txtPhone.Text.Trim());
+            sqlCmd.ExecuteNonQuery();  //executes the transcation that wants to be done
+            sqlCon.Close();  //close connection
+            string iD = hf.Value; //savind id value
+            clear();   //clear the fields
+            if (iD == "")  // this the case when there is a new entry
+            {
+                lblSuccessMessage.Text = "Saved Succesfuly";
+                FillGridView();
+            }
+            else   // this is the case when entry is udate
+            {
+                lblSuccessMessage.Text = "Updated Successfully";
+                FillGridView();
+            }
+        }
+    }
+
+    void FillGridView()   // shows data in gridview
+    {
+        if (sqlCon.State == ConnectionState.Closed)
+        {
+            sqlCon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("ContactViewAll", sqlCon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dtb = new DataTable();
+            sqlDa.Fill(dtb);
+            sqlCon.Close();
+            gvCotact.DataSource = dtb;
+            gvCotact.DataBind();
+
+        }
+    }
+    protected void lnkOnClick(object sender, EventArgs e) //for viewing data in the fields in
+    {
+        int Id = Convert.ToInt32((sender as LinkButton).CommandArgument);
+        if (sqlCon.State == ConnectionState.Closed)
+        {
+            sqlCon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("ContactViewById", sqlCon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDa.SelectCommand.Parameters.AddWithValue("@Id", Id);
+            DataTable dtb = new DataTable();
+            sqlDa.Fill(dtb);
+            sqlCon.Close();
+            hf.Value = Id.ToString();
+            txtFirst.Text = dtb.Rows[0]["First"].ToString();
+            txtLast.Text = dtb.Rows[0]["Last"].ToString();
+            txtPhone.Text = dtb.Rows[0]["Phone"].ToString();
+
+            btnSave.Text = "Update";
+            btnDelete.Enabled = true;
+
+        }
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e) //deleting entry from db
+    {
+        if (sqlCon.State == ConnectionState.Closed)
+        {
+            sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand("ContactDeleteById", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@Id", Convert.ToInt32(hf.Value));
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            clear();
+            FillGridView();
+            lblSuccessMessage.Text = "Deleted Successfully";
+        }
+    }
+}
